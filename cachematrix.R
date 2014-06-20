f729253793b71d95b13d08a4506a55bceff33afe
Cvetanka <Cvetanka@users.noreply.github.com>
2014-06-21 00:28:11 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
 
 
 ## Write a short comment describing this function
-  ## Set up a matrix x in a nullspace N(x); Some arbitrary matrix A is defined in NULL space N(A)
+  ## Initialize a matrix x in a nullspace N(x) with a command set; Some arbitrary matrix A is defined in NULL space N(A)
   ## if the following equation is satisfied Ax = 0, where x is set of vectors that satisfy this equation.
-  ## Get a value of the matrix
-  ## Set a inverse of the matrix x; if x is a square matrix than solve() will give the inverse(x), denoted as m
+  ## Assign some value of the matrix with the command get
+  ## Calculate the inverse of the matrix x; if x is a square matrix than solve() will give the inverse(x), denoted as m here
   ## For more general cases ginv(x) is the right solution. Moore-Penrose Generalized Inverse of x
   ## ginv() requires loading the MASS package. 
   ## The inverse value of the matrix is deposited in the cache. The operator <<- assign the value of the object in
@@ -50,15 +48,19 @@ cacheSolve <- function(x, ...) {
 ## Following the instructions given for vectorMean function in one of the discussions forums 
 ## I have tested the cacheMatrix function.
 ## Make the matrix b using cacheMatrix function:
+
 ## > b <- makeCacheMatrix(matrix(c(3,5,8,1,0,7,65,98,2),3,3))
+
 ## deposit the inverse of the matrix b in cache by:
 ## > cacheSolve(b)
 ##          [,1]        [,2]        [,3]
 ## [1,] -0.69223007  0.45711403  0.098890010
 ## [2,]  0.78102926 -0.51866801  0.031281534
 ## [3,]  0.03531786 -0.01311806 -0.005045409
+
 ## call b
 ## > b
+
 ## >$set
 ## function (y) 
 ## {
@@ -81,6 +83,7 @@ cacheSolve <- function(x, ...) {
 ## function () 
 ##  m
 ## <environment: 0x000000000a7ee938>
+
 ## call aagain cacheSolve function to see if I will get the inverse from the cache
 ## > cacheSolve(b)
 ## and received the message: getting cached data
